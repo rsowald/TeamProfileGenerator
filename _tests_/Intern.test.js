85d91642738bfae667b693ef27326624b5714f48
rsowald <72467920+rsowald@users.noreply.github.com>
2021-02-28 23:21:57 -0800

    wrote intern tests

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,14 @@
+const Intern = require('../lib/Intern');
+
+describe('Intern class', () => {
+
+
+    describe('constructor and getter methods', () => {
+        const intern = new Intern('Shree Ravikumar', 625, 'ShreeRavi625@fakemail.com', 'OSU');
+        expect(intern.getName().toBe('Shree Ravikumar'));
+        expect(intern.getId().toBe(625));
+        expect(intern.getEmail().toBe('ShreeRavi625@fakemail.com'));
+        expect(intern.getSchool().toBe('OSU'));
+        expect(intern.getRole().toBe('Intern'));
+    });
+})
