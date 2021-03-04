47c553cdda9b9b095a38415582e819be9c73db36
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-04 00:35:15 -0800

    fixed parenthesis errors on tests, added role parameter to employee test, and added snapshot on intern test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,11 +3,11 @@ const Employee = require('../lib/Employee');
 describe('Employee class', () => {
 
 
-    describe('constructor and getter methods', () => {
-        const employee = new Employee('John Doe', 458, 'doe458@fakemail.com');
-        expect(employee.getName().toBe('John Doe'));
-        expect(employee.getId().toBe(458));
-        expect(employee.getEmail().toBe('doe458@fakemail.com'));
-        expect(employee.getRole().toBe('Employee'));
+    it('constructor and getter methods', () => {
+        const employee = new Employee('John Doe', 458, 'doe458@fakemail.com', 'Employee');
+        expect(employee.getName()).toBe('John Doe');
+        expect(employee.getId()).toBe(458);
+        expect(employee.getEmail()).toBe('doe458@fakemail.com');
+        expect(employee.getRole()).toBe('Employee');
     });
-})
+});
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -5,10 +5,15 @@ describe('Intern class', () => {
 
     describe('constructor and getter methods', () => {
         const intern = new Intern('Shree Ravikumar', 625, 'ShreeRavi625@fakemail.com', 'OSU');
-        expect(intern.getName().toBe('Shree Ravikumar'));
-        expect(intern.getId().toBe(625));
-        expect(intern.getEmail().toBe('ShreeRavi625@fakemail.com'));
-        expect(intern.getSchool().toBe('OSU'));
-        expect(intern.getRole().toBe('Intern'));
+        expect(intern.getName()).toBe('Shree Ravikumar');
+        expect(intern.getId()).toBe(625);
+        expect(intern.getEmail()).toBe('ShreeRavi625@fakemail.com');
+        expect(intern.getSchool()).toBe('OSU');
+        expect(intern.getRole()).toBe('Intern');
+    });
+
+    it('intern card', () => {
+        const intern = new Intern('Shree Ravikumar', 625, 'ShreeRavi625@fakemail.com', 'OSU');
+        expect(intern.getEmployeeCard()).toMatchSnapshot();
     });
 })
