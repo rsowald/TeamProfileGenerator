bd19b57758c0b004e98171a779eb1e28322b16fe
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-04 00:15:35 -0800

    fix engineer test and added snapshot

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -3,12 +3,19 @@ const Engineer = require('../lib/Engineer');
 describe('Engineer class', () => {
 
 
-    describe('constructor and getter methods', () => {
-        const engineer = new Engineer('John Doe', 458, 'doe458@fakemail.com', 'JDoe1976');
-        expect(engineer.getName().toBe('John Doe'));
-        expect(engineer.getId().toBe(458));
-        expect(engineer.getEmail().toBe('doe458@fakemail.com'));
-        expect(engineer.getGitHub().toBe('JDoe1976'));
-        expect(engineer.getRole().toBe('Engineer'));
+    it('constructor and getter methods', () => {
+        const engineer = new Engineer('John Doe', 458, 'doe458@fakemail.com', 'https://github.com/jdozer');
+        expect(engineer.getName()).toEqual('John Doe');
+        expect(engineer.getId()).toBe(458);
+        expect(engineer.getEmail()).toBe('doe458@fakemail.com');
+        expect(engineer.getGitHub()).toBe('https://github.com/jdozer');
+        expect(engineer.getRole()).toBe('Engineer');
+    });
+
+
+
+    it('engineer card', () => {
+        const engineer = new Engineer('John Doe', 458, 'doe458@fakemail.com', 'https://github.com/jdozer');
+        expect(engineer.getEmployeeCard()).toMatchSnapshot();
     });
 })
